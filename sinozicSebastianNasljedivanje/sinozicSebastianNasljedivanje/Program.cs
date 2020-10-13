71a713ac8cbec965ece562ea45565887dac0c84a
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-10-13 10:55:09 +0200

    Add files via upload

---
+++ sinozicSebastianNasljedivanje/sinozicSebastianNasljedivanje/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianNasljedivanje
+{
+
+    interface IControl
+    {
+        void Metoda();
+
+        int Brojac { get; set; }
+
+    }
+
+    class ListBox : IControl
+    {
+
+
+        public void Metoda()
+        {
+            Console.WriteLine("Metoda List");
+        }
+
+        public int Brojac
+        {
+            get { return Brojac; }
+            set { Brojac = value; }
+        }
+
+       
+
+
+
+        class ComboBox : IControl
+        {
+            public int Brojac { get; set; }
+
+            public void Metoda()
+            {
+                Console.WriteLine("Metoda Combo");
+
+            }
+        }
+
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                ListBox x = new ListBox();
+                ComboBox y = new ComboBox();
+
+                x.Metoda();
+                y.Metoda();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
